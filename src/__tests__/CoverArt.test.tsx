964e165752ec869ba17089ed682188290d1f59f0
Allison <allisonabinger@gmail.com>
2024-09-12 09:50:15 -0500

    A - 09/12 09:50 AM - Created empty test files, ran CoverArt tests

---
+++ src/__tests__/CoverArt.test.tsx
@@ -0,0 +1,23 @@
+// Test Suite for the CoverArt Component
+
+import { render } from "@testing-library/react";
+import { expect, test } from "vitest";
+import CoverArt from "../components/CoverArt";
+import SoulCanvas from "../assets/SoulCanvas.jpeg";
+
+// Snapshot Tests for CoverArt
+
+test("CoverArt renders an image", () => {
+  const { container } = render(<CoverArt imageURL={SoulCanvas}/>);
+  expect(container).toMatchSnapshot();
+});
+
+test("CoverArt will not render without a URL", () => {
+    const { container } = render(<CoverArt imageURL="" />);
+    expect(container).toMatchSnapshot();
+});
+
+test("CoverArt will not render without a valid URL", () => {
+    const { container } = render(<CoverArt imageURL="525600" />);
+    expect(container).toMatchSnapshot();
+})
